cfbc0226e15c4352f17da0e95c6c30eb372e3085
Brenden Matthews <github@brenden.brndn.io>
2024-03-19 14:37:09 -0400

    New special effects 🔥🔥🔥

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,9 @@ module.exports = {
           },
         },
       }),
+      animation: {
+        "pulse-fast": "pulse 1s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
     },
   },
   plugins: [require("@tailwindcss/typography")],
